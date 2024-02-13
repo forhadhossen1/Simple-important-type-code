df1fafeb30a4837badcaa87ffa7942294bdd5c8c
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-13 16:48:23 +0600

    added tailwind components daisy ui

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-primary border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
