35f0e330e7565ee63f0110cc2eb7feafe31572d5
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-17 01:36:12 +0600

    setup framer-motion

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion"
+
+const Hero = () => {
+    return (
+        <div>
+            Yesh I am hero section
+        </div>
+    );
+};
+
+export default Hero;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
+import Hero from "../Components/Hero";
 
 const Home = () => {
     return (
         <div>
+            <Hero></Hero>
             <p>yesh You stil stay home okkkk... </p>
         </div>
     );
